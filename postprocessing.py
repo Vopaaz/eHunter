4ff409cf90b0def29f8a55027dedc7673e7c983e
Vopaaz <liyifan945@gmail.com>
2023-11-05 10:38:56 -0800

    Add additional fields

--- postprocessing.py
+++ postprocessing.py
@@ -8,17 +8,19 @@ with open(inject_js_path, "r") as f:
 metadata = """\
 // ==UserScript==
 // @name         eHunter-vopaaz
-// @namespace    http://tampermonkey.net/
-// @version      2.7.3.1
 // @description  Modifies several default settings for https://github.com/hanFengSan/eHunter to use in incognito
+// @copyright    2023, Vopaaz
+// @license      MIT
+// @homepageURL  https://github.com/Vopaaz/eHunter
 // @supportURL   https://github.com/Vopaaz/eHunter
+// @contributionURL   https://github.com/Vopaaz/eHunter
+// @updateURL    https://openuserjs.org/meta/Vopaaz/eHunter-vopaaz.meta.js
+// @version      2.7.3.1
 // @author       Vopaaz
 // @match        https://exhentai.org/*
 // @match        https://e-hentai.org/*
 // @match        https://nhentai.net/*
 // @grant        GM_xmlhttpRequest
-// @license      MIT
-// @updateURL    https://openuserjs.org/meta/Vopaaz/eHunter-vopaaz.meta.js
 // ==/UserScript==
 
 """
